7ca9557650d8b5097e4c766a07dcb32ae70b1c6a
Alyssandro <dyogo.alyssandro@gmail.com>
2025-07-20 19:44:19 -0300

    feat: permission of the cross origin

--- server.py
+++ server.py
@@ -3,12 +3,18 @@ import sanic.response
 import os
 from dotenv import load_dotenv
 from db.mongo import init_db
+from sanic_cors import CORS
+from sanic_ext import Extend
 
 from routes.health_report_route import state_bp
 
 load_dotenv()
 
 app = sanic.Sanic("app")
+app.config.CORS_ORIGINS = os.getenv("FRONTEND_URL")
+Extend(app)
+
+CORS(app, resources={r"/*": {"origins": os.getenv("FRONTEND_URL")}}, automatic_options=True, supports_credentials=True)
 
 @app.listener("before_server_start")
 async def setup_db(app, loop):
@@ -16,7 +22,7 @@ async def setup_db(app, loop):
 
 @app.route("/")
 async def index(request):
-    return sanic.response.text("Server stating up...")
+    return sanic.response.text("Server starting up...")
 
 # Blueprints da aplicação
 app.blueprint(state_bp)
