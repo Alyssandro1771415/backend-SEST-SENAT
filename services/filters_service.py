983220e064f0171187d2000fc73251c371854e28
Alyssandro <dyogo.alyssandro@gmail.com>
2025-07-25 12:46:46 -0300

    feat: implementation of new function to get just the filters

--- services/filters_service.py
+++ services/filters_service.py
@@ -1,4 +1,5 @@
 from models.patient_model import Patient
+from models.consultations_model import Consultation
 from beanie.operators import And
 
 async def get_region_health_datas_filters(regiao: str, filters: dict):
@@ -11,6 +12,8 @@ async def get_region_health_datas_filters(regiao: str, filters: dict):
     if filters:
         if filters.get("unidade"):
             query_parts.append(Patient.unidade == filters["unidade"][0])
+        if filters.get("ano"):
+            query_parts.append(Patient.ano == filters["ano"][0])
         if filters.get("idade"):
             query_parts.append(Patient.faixa_etaria == filters["idade"][0])
         if filters.get("genero"):
@@ -36,3 +39,31 @@ async def get_region_health_datas_filters(regiao: str, filters: dict):
     all_datas = [p.model_dump(exclude={"id"}) for p in patients_filtered_datas]
 
     return all_datas
+
+async def get_region_consultations_filters(regiao: str, filters: dict):
+    query_parts = []
+
+    if regiao != "all":
+        query_parts.append({"Região": regiao})
+
+    if filters:
+        if filters.get("unidade"):
+            query_parts.append({"UnidadeAtendimento": filters["unidade"][0]})
+        if filters.get("ano"):
+            query_parts.append({"Ano": filters["ano"][0]})
+        if filters.get("idade"):
+            query_parts.append({"FaixaEtaria": filters["idade"][0]})
+        if filters.get("genero"):
+            query_parts.append({"Genero": filters["genero"][0]})
+        if filters.get("conselho"):
+            query_parts.append({"ConselhoRegional": filters["conselho"][0]})
+        if filters.get("estado"):
+            query_parts.append({"UF": filters["estado"][0]})
+        if filters.get("modal"):
+            query_parts.append({"Modal": filters["modal"][0]})
+        if filters.get("cnpj"):
+            query_parts.append({"CNPJ": str(filters["cnpj"][0]).zfill(14)})
+        if filters.get("nome_empresa"):
+            query_parts.append({"NomeEmpresa": filters["nome_empresa"][0]})
+
+    return query_parts
